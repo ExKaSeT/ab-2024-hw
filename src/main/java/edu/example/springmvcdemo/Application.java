5fe288a4aba4d2020931cc2a1d9d7ddc62017c66
ExKaSeT <exkaset@mail.ru>
2024-02-27 22:18:31 +0300

    hw2

--- src/main/java/edu/example/springmvcdemo/Application.java
+++ src/main/java/edu/example/springmvcdemo/Application.java
@@ -2,7 +2,15 @@ package edu.example.springmvcdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableCaching
+@EnableMongoRepositories
+@EnableJpaRepositories(excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".*dao.mongo.*"))
 @SpringBootApplication
 public class Application {
 
--- src/main/java/edu/example/springmvcdemo/model/Message.java
+++ src/main/java/edu/example/springmvcdemo/model/Message.java
@@ -2,16 +2,34 @@ package edu.example.springmvcdemo.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Data
-@Table(name = "message")
+@Table(name = "messages")
 public class Message {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "message_seq")
-    @SequenceGenerator(name = "message_seq", sequenceName = "message_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "messages_seq")
+    @SequenceGenerator(name = "messages_seq", sequenceName = "messages_seq", allocationSize = 1)
     private Long id;
 
-    @Column(name = "content")
+    @Column(name = "content", length = 300)
     private String content;
+
+    @Column(name = "last_modified")
+    @UpdateTimestamp
+    private LocalDateTime lastModifiedDate;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @JoinTable(
+            name = "messages_images",
+            joinColumns = @JoinColumn(name = "message_id"),
+            inverseJoinColumns = @JoinColumn(name = "image_link"))
+    private List<Image> images;
 }
