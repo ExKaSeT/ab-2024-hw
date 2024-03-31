b3577d8970ce62afee25dc29a56d9db39a1107f5
ExKaSeT <exkaset@mail.ru>
2024-03-31 22:28:03 +0300

    refactor

--- src/main/java/edu/example/springmvcdemo/exception/handler/AppExceptionHandler.java
+++ src/main/java/edu/example/springmvcdemo/exception/handler/AppExceptionHandler.java
@@ -22,23 +22,17 @@ public class AppExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<Object> handleException(ConstraintViolationException ex) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("errors", List.of(ex.getMessage()));
-        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(createBody(ex), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<Object> handleException(DataIntegrityViolationException ex) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("errors", List.of(ex.getRootCause().getMessage()));
-        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(createBody(ex), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException ex) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("errors", List.of(ex.getMessage()));
-        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(createBody(ex), HttpStatus.NOT_FOUND);
     }
 
     @Override
@@ -53,5 +47,9 @@ public class AppExceptionHandler extends ResponseEntityExceptionHandler {
                         .filter(s -> !s.isBlank())
                         .toList());
         return new ResponseEntity<>(body, status);
+    }
+
+    private Map<String, Object> createBody(Throwable exception) {
+        return new HashMap<>(Map.of("errors", List.of(exception.getMessage())));
     }
 }
