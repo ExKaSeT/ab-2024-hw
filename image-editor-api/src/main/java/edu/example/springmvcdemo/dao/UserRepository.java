58474a1b414451db6151023ae84de71133c1219e
ExKaSeT <exkaset@mail.ru>
2024-05-12 22:33:50 +0300

    add dependencyManagement and multi-module test report

--- image-editor-api/src/main/java/edu/example/springmvcdemo/dao/UserRepository.java
+++ image-editor-api/src/main/java/edu/example/springmvcdemo/dao/UserRepository.java
@@ -4,12 +4,10 @@ package edu.example.springmvcdemo.dao;
 import edu.example.springmvcdemo.model.Role;
 import edu.example.springmvcdemo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long>, QuerydslPredicateExecutor<User> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
 
